47487ca2773430b75118e493dff05bd093e25c38
KhoiDOO <khoido8899@gmail.com>
2023-08-28 15:42:07 +0700

    update omniglot model

--- classification/omniglot/omniglot.py
+++ classification/omniglot/omniglot.py
@@ -48,7 +48,7 @@ transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5
 trainset = Omniglot(root='./data', background=True, download=True, transform=transform)
 train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
 
-model = CNN(30)
+model = CNN(964)
 optimizer = torch.optim.Adam(model.parameters(), lr)
 criterion = torch.nn.CrossEntropyLoss()
 scheduler = CosineAnnealingLR(optimizer, len(train_loader)*epochs)
